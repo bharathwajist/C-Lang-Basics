81db5a1e11bcd51234768bb8f7750b2b79dfc8de
Bharathwaj <41534996+bharathwajist@users.noreply.github.com>
2022-01-16 18:47:16 +0530

    Add files via upload

---
+++ MathFunction.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+    printf("Square Root of 30 is : %f\n",sqrt(30));
+    printf("Cube Root of 30 is : %f\n",cbrt(30));
+    printf("Exponent of 30 is : %f\n",exp(30));
+    printf("Log of 30 is : %f\n",log(30));
+    printf("Log10 of 30 is : %f\n",log10(30));
+    printf("Fabs of 30 is : %f\n",fabs(30));
+    printf("Ceil of 30 is : %f\n",ceil(30));
+    printf("Floor of 30 is : %f\n",floor(30));
+    printf("Power of 30^2 is : %f\n",pow(30,2));
+}
