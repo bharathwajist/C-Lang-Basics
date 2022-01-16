c1426882e29ddd8a8208d68307c14fc0ff314ab4
Bharathwaj <41534996+bharathwajist@users.noreply.github.com>
2022-01-16 16:33:14 +0530

    Add files via upload

---
+++ CompoundInt.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    double principal = 1000.0;
+    double rate = 0.05;
+
+    printf("%4s%21s\n","Year","Amount on deposit");
+    for(unsigned int year = 1; year <= 10; ++year){
+        double amount = principal * pow(1.0+rate,year);
+        printf("%4u%21.2f\n",year,amount);
+    }
+}
