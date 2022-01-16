718537623a21e14edd1bef0954e25a655baa357c
Bharathwaj <41534996+bharathwajist@users.noreply.github.com>
2022-01-16 16:32:49 +0530

    Add files via upload

---
+++ CalculateThreeInt.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void){
+    printf("Enter three integers : \n");
+    int x,y,z;
+    scanf("%d%d%d",&x,&y,&z);
+
+    int result = x*y*z;
+    printf("The product is %d\n",result);
+}
