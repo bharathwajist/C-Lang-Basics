24c2233f3b1be6879ae1cbaf742cb72209868f5e
Bharathwaj <41534996+bharathwajist@users.noreply.github.com>
2022-01-17 18:34:11 +0530

    Add files via upload

---
+++ Float.c
@@ -0,0 +1,11 @@
+// Implementing Float.h in C language
+
+#include <stdio.h>
+#include <float.h>
+
+int main(){
+    printf("The maximum value of float = %.10e\n",FLT_MAX);
+    printf("The minimum value of float = %.10e\n",FLT_MIN);
+
+    printf("The number of digits in the number = %.10d\n",FLT_MANT_DIG);
+}
