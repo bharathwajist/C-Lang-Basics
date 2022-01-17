1a2970e22df7fffdb3b8db5940b705b43b72daeb
Bharathwaj <41534996+bharathwajist@users.noreply.github.com>
2022-01-17 18:35:04 +0530

    Add files via upload

---
+++ SetLocale.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <time.h>
+#include <locale.h>
+
+int main(){
+    time_t currtime;
+    struct tm *timer;
+    char buffer[80];
+
+    time(&currtime);
+    timer = localtime(&currtime);
+
+    printf("Locale is : %s\n",setlocale(LC_ALL,"en_GB"));
+    strftime(buffer,80,"%c",timer);
+    printf("Date is : %s\n",buffer);
+
+    printf("Locale is : %s\n",setlocale(LC_ALL,"en_IN"));
+    strftime(buffer,80,"%c",timer);
+    printf("Date is : %s\n",buffer);
+
+    printf("Locale is : %s\n",setlocale(LC_ALL,"de_DE"));
+    strftime(buffer,80,"%c",timer);
+    printf("Date is : %s\n",buffer);
+    return 0;
+}
